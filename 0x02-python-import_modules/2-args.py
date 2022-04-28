bdc9c9d3f1bb79e646fae62c798ad09569b46c9a
root <root@LAPTOP-I8GVERUB.localdomain>
2022-04-28 16:28:36 +0200

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     print("{} ".format(len(argv) - 1), end='')
-    
     if len(argv) == 1:
         print("arguments.")
     elif len(argv) == 2:
         print('argument:')
     else:
-        print('arguments:')
-    
+        print('arguments:') 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
