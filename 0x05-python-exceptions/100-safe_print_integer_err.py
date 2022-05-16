444f010b71e2d8736729a5d6707d143c5ffb0a2f
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-16 20:10:49 +0200

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,11 @@
 import sys
 
 
-def safe_print_integer_err(value):
+def safe_print_integer_err(value
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
-        print(file=sys.stderr, end="")
+    except Exception as e:
+        import sys
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
