db2cb310534a33578512b209d02e20f78b132792
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-17 22:38:26 +0200

    test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,57 +1,55 @@
 #!/usr/bin/python3
-""" Defining a Square """
-
-
 class Square:
-    """Square class
-    Attributes: size (int): size of the squre """
+    """class that defines a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """size initialization
-        position initialization"""
-        self.size = size
+        """inisialising"""
+        self.__size = size
         self.position = position
-
-    def area(self):
-        """Calculate the area of the square"""
-        return self.__size ** 2
 
     @property
     def size(self):
-        """ Retrieve size"""
+        """do size"""
         return self.__size
+
+    @property
+    def position(self):
+        """getting position"""
+        return self.__position
 
     @size.setter
     def size(self, value):
-        """Setter of size"""
-        if type(value) is not int:
+        """set size"""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
+            """return the square"""
             self.__size = value
-
-    def my_print(self):
-        """Printing a square"""
-        if self.size == 0:
-            print("")
-        else:
-            print("\n" * self.__position[1], end="")
-            for i in range(self.size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
-
-    @property
-    def position(self):
-        """ Retrieve """
-        return self.__position
 
     @position.setter
     def position(self, value):
-        """ set the position """
-        if type(value) == tuple and len(value) == 2 and \
-           type(value[0]) is int and value[0] >= 0 and \
-           type(value[1]) is int and value[1] >= 0:
-            self.__position = value
+        """set position"""
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            raise("position must be a tuple of 2 positive integers")
+            self.__position = value
+
+    def area(self):
+        """getting size square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """printing a square"""
+        if self.size > 0:
+            if self.position[1] > 0:
+                print("\n" * self.position[1], end="")
+            for i in range(self.size):
+                if self.position[0] > 0:
+                    print(" " * self.position[0], end="")
+                print("#" * self.size)
+        else:
+            print(end="\n")
