d21c87ec2aa081dfd709a4c65468d1be01c81b98
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-17 20:40:06 +0200

    test

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,4 +4,6 @@
 
 
 class Square:
+    """""empty square
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-""" class  that defines a square
-"""
+""" a class  that defines a square."""
 
 
 class Square:
-    pass
+    """empty Square"""
 
-
-"""initialsation
-"""
-
-
-def __init__(self, size):
-    self.size = size
+    def __init__(self):
+        return
