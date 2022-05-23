a18cead9be4e82f5622da50ebaa4bf177edd1bb4
root <root@LAPTOP-I8GVERUB.localdomain>
2022-05-23 22:16:32 +0200

    unit testing

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
     def __str__(self):
         """return print of the rectangle"""
         string = ""
-        if self.__width != 0 and self.__height != 0:
+        if self.__width == 0 and self.__height == 0:
             string += "\n".join("#" * self.__width
                                 for j in range(self.__height))
         return string
