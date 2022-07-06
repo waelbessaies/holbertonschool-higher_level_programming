8c75f0273fd0c13353b8147bcf82954d56448e90
root <root@LAPTOP-I8GVERUB.localdomain>
2022-07-06 16:36:55 +0200

    sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
---a script that creates the table id_not_null on your MySQL server.
+-- a script that creates the table id_not_null on your MySQL server.
 CREATE TABLE IF NOT EXISTS id_not_null
 (
     id  INT(1),
