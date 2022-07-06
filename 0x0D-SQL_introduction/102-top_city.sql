81e227b2227378bccf269da242727306713d9ee0
root <root@LAPTOP-I8GVERUB.localdomain>
2022-07-06 22:29:41 +0200

    test

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- a script that displays the top 3 of cities temperature during July and August ordered by temperature 
+SELECT city,
+AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 8 OR month = 7 
+GROUP BY city
+ORDER BY avg_temp DESC LIMIT 3;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- max temperature
+SELECT state,
+MAX(value) as max_temp
+FROM temperatures 
+GROUP BY state
+ORDER BY state ASC;
