8b2d4a57da044fc1fb45eda6d49ba48b7911da37
root <root@LAPTOP-I8GVERUB.localdomain>
2022-09-09 20:55:49 +0200

    tasks

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,6 @@
 for x in range(0, 99):
     if x % 10 > x / 10:
         if x != 89:
-            print(f"{x:02d}, ", end='')
             print("{:02d}, ".format(x), end='')
         else:
-            print(f"{x:02d}")
             print("{:02d}".format(x))
