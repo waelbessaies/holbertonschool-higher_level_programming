087c7495b95565d3a30e0041115d72a04b4732c7
root <root@LAPTOP-I8GVERUB.localdomain>
2022-09-09 17:34:10 +0200

    last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+S = ("Last digit of")
+if number >= 0:
+    ld = number % 10
+if number < 0:
+    ld = number % -10
+if ld > 5:
+    print(f"{S} {number} is {ld} and is greater than 5")
+elif ld == 0:
+    print(f"{S} {number} is {ld} and is 0")
+elif ld < 6 and ld != 0:
+    print(f"{S} {number} is {ld} and is less than 6 and not 0")
