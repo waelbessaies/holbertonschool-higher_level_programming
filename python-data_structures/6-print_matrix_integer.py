2de999691715bae9c5d1941dca21ae3c38cc6892
root <root@LAPTOP-I8GVERUB.localdomain>
2022-09-15 16:13:06 +0200

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-         for element in row:
+        for element in row:
             print(" ".join( "{:d}".format(element) ))
+print()
