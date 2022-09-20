19354a1010ae03f0721f7341760af0cd754d927e
root <root@LAPTOP-I8GVERUB.localdomain>
2022-09-20 22:39:22 +0200

    Write a class Square that defines a square by: (based on 0-square.py)

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,7 +9,6 @@ class Square:
      class Square that defines a square
     """
 
-
     def __init__(self, size):
         """ Initializing square , size: size of the side of the Square"""
         self.__size = size
