669429ff847f90383d18a630f468d936190026e0
root <root@LAPTOP-I8GVERUB.localdomain>
2022-09-21 20:43:47 +0200

    function  that prints a square with the character #

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function  that prints a square with the character #
+"""
+
+
+def print_square(size):
+    """prints a square with the character #"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print('#', end="")
+            print("")
