f2a386fe8cc7bfc5361d3dd1e2b7ec80bf5149bf
root <root@LAPTOP-I8GVERUB.localdomain>
2022-09-21 20:54:51 +0200

    function  that prints a square with the character #

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
     if type(size) is not int:
         raise TypeError("size must be an integer")
     elif size < 0:
-        raise ValueError("size must be >= 0")
+        raise TypeError("size must be >= 0")
     else:
         for i in range(size):
             for j in range(size):
