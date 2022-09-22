bf1f313c1bee95cf0428067713f2365ace8bd4cd
root <root@LAPTOP-I8GVERUB.localdomain>
2022-09-22 17:05:25 +0200

    Write a class Rectangle that defines a rectangle by: (based on 3-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -58,67 +58,6 @@ class Rectangle:
             return 0
         return (self.__height + self.__width) * 2
 
-
-#!/usr/bin/python3
-"""
- class that create a Rectangle
-"""
-
-
-class Rectangle:
-    """
-    empty class Square that defines a Rectangle.
-    """
-
-    def __init__(self, width=0, height=0):
-        """ Initializing Rectangle
-        """
-        self.height = height
-        self.width = width
-
-    @property
-    def width(self):
-        """ Retrieve width """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Set the width value """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """ Retrieve height value """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Set the height value """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """ Class Method that calculates the Area of the rectangle
-        Return:
-            The area of the rectangle
-        """
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """ Class Method that calculates the perimeter of the rectangle
-        Return:
-            The perimeter of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return (self.__height + self.__width) * 2
-
     def __str__(self):
         """__str__ method
         this method will print the rectangle on print() or str()
@@ -132,7 +71,7 @@ class Rectangle:
             if i is not (self.__height - 1):
                 my_str += "\n"
         return my_str
-    
+
     def __repr__(self):
         """return a string representation of the rectangle
         """
