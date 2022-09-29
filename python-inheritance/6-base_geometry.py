72c746cf044a1b2803bafa75350f62d96026d714
root <root@LAPTOP-I8GVERUB.localdomain>
2022-09-29 17:56:20 +0200

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Task 6'''
+
+
+class BaseGeometry:
+    '''This is is a class'''
+
+    def area(self):
+        ''' a function'''
+        raise Exception("area() is not implemented")
