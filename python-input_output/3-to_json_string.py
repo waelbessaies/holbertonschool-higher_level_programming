c58f075db238ac161533a6dd54d567a83947912a
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-05 14:40:53 +0200

    function that returns the JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''A module that representes a function that returns
+the JSON representation of an object'''
+import json
+
+
+def to_json_string(my_obj):
+    ''' a function that returns the JSON representation of an object (string):'''
+    return json.dumps(my_obj)
