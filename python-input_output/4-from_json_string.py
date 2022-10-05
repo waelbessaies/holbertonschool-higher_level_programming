a7e59dec1fe62bc81d43ffb4faced2f6935e935b
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-05 14:58:10 +0200

    a function that returns an object (Python data structure) represented by a JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''This is a moudle'''
+import json
+
+
+def from_json_string(my_str):
+    ''' a function that returns an object (Python data structure) represented by a JSON string'''
+    return json.loads(my_str)
