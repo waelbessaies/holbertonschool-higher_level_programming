afc1c9f40185a4d7eb7223b288cb4bd7e8d46e11
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-05 14:21:01 +0200

    write a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     '''a function that writes a text file'''
-     with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         file.write(text)
     return len(text)
