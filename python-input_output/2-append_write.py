bbd3b4deea2beea068eb4578fb9d7b01ec35dc22
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-05 14:27:45 +0200

    append a string

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-'''Task 2'''
+'''Write a function that appends a string at the end of a text file (UTF8)
+        and returns the number of characters added'''
 
 
 def write_file(filename="", text=""):
-    '''append a string at eof'''
+'''Write a function that appends a string at the end of a text file (UTF8)
+        and returns the number of characters added:'''
     with open(filename, "a", encoding="utf-8") as file:
         file.write(text)
     return len(text)
