11143674537d2952a0f710c1b3eff7056ce4c387
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-05 14:50:18 +0200

    append a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""  a module that representes a function
-that appends a string to a text file """
+"""
+a module that representes a function
+that appends a string to a text file
+"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
-    A function that appends a string to the end of a text file
-    and returns the number of characters added
+    a function that appends a string to the end of a text file
+    and returns the number of characters
     """
-    with open(filename, "a", encoding="utf-8") as file:
-    file.write(text)
-    return len(text)
+    with open(filename, mode="a", encoding="utf-8") as file:
+        return file.write(text)
