b7409d5598ba5d28708759bfe283f4fe5b7a683e
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-05 14:36:12 +0200

    append a string

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,12 @@
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file and return the number of characters"""
+    """writes a string to a text file and return the number of characters
+    args:
+        filename: the name of the textfile
+        text: the string to be inserted in the textfile
+    return:
+         number of char written into the textfile"""
     with open(filename, "a", encoding="utf-8") as file:
         file.write(text)
     return len(text)
