67ed106b816ef18e73726c9c4521663a63d97587
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-05 14:23:47 +0200

    append a string

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Task 1'''
+
+
+def write_file(filename="", text=""):
+    '''a function that writes a text file'''
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(text)
+    return len(text)
