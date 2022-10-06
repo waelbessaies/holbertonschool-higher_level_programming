9315f70e0a5d2ebde47f27c872d7730bfc47ed34
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-06 13:28:42 +0200

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,5 +76,4 @@ class Rectangle(Base):
 
     def __str__(self):
         return "[Rectangle] (<id>) <x>/<y> - <width>/<height>".format(
-            self.id, self.x, self.y, self.width, self.height
-        )
+            self.id, self.x, self.y, self.width, self.height)
