3c0c12eb2a036b8f5649db956c00870053a5e0c1
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-06 13:31:45 +0200

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,5 +75,5 @@ class Rectangle(Base):
             print("")
 
     def __str__(self):
-        return "[Rectangle] (<>) <>/<> - <>/<>".format(
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
