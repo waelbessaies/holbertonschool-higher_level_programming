ed8a0dcdf1dcb7070d9547e824623ed544001239
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-27 20:40:31 +0200

    input output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ Adds all arguments to a Python list"""
-import sys
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
-        __import__('6-load_from_json_file').load_from_json_file
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        my_list = []
 
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, "add_item.json")
+try:
+    new_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    new_list = []
+
+i = 1
+if  len(argv) >0  != 1:
+    while i < len(argv):
+        new_list.append(argv[i])
+        i += 1
+save_to_json_file(new_list, "add_item.json")
