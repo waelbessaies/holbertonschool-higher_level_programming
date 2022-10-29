522afef38e519a2343bc1b934e6170d7ba127239
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-29 14:12:20 +0200

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """class"""
+import json
 
 
 class Base:
-    """def function init """
+    """function init"""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -12,3 +13,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+
+@staticmethod
+def to_json_string(list_dictionaries):
+    """update the class Base by adding the static methoddef to_json_string"""
+    if list_dictionaries is None:
+        return "[]"
+    else:
+        return json.dumps(list_dictionaries)
+@staticmethod
+def from_json_string(json_string):
+    """method that write the list of the JSON from json string"""
+    if json_string is None:
+        return []
+    else:
+        return json.loads(json_string)
