a73483aa80984170ab3a1c7e85bfea8bf728c38c
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-29 14:22:09 +0200

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,19 +15,19 @@ class Base:
             self.id = self.__nb_objects
 
 
-@staticmethod
-def to_json_string(list_dictionaries):
-    """returns the JSON string representation of list_dictionaries:"""
-    if list_dictionaries is None:
-        return "[]"
-    else:
-        return json.dumps(list_dictionaries)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries:"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
 
 
-@staticmethod
-def from_json_string(json_string):
-    """eturns the list of the JSON string representation json_string:"""
-    if json_string is None:
-        return []
-    else:
-        return json.loads(json_string)
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string:"""
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
