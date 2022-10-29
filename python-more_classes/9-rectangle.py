d8522849c992b8219e05e599ddf6b172f8b7ae4b
root <root@LAPTOP-I8GVERUB.localdomain>
2022-10-29 14:58:56 +0200

    9-rectangle.py

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -12,59 +12,13 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        """ Initializing Rectangle
-        """
-        self.height = height
+        """__init__ method"""
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
-    @property
-    def width(self):
-        """ Retrieve width """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Set the width value """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """ Retrieve height value """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Set the height value """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """ Class Method that calculates the Area of the rectangle
-        Return:
-            The area of the rectangle
-        """
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """ Class Method that calculates the perimeter of the rectangle
-        Return:
-            The perimeter of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return (self.__height + self.__width) * 2
-
     def __str__(self):
-        """__str__ method
-        this method will print the rectangle on print() or str()
-        """
+        """__str__ method"""
         my_str = ""
         if self.__width == 0 or self.__height == 0:
             return my_str
@@ -75,15 +29,56 @@ class Rectangle:
                 my_str += "\n"
         return my_str
 
-    def __repr__(self):
-        """return a string representation of the rectangle
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """area Method
+        calculate the rectangle object's area
         """
-        return ('Rectangle({}, {})'.format(self.__width, self.__height))
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """perimeter Method
+        calculate the rectangle object's perimeter
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __repr__(self):
+        """
+        Return the representation of the Rectangle.
+        """
+        strWidth = str(self.width)
+        strHeight = str(self.height)
+        return f"Rectangle({strWidth}, {strHeight})"
 
     def __del__(self):
-        ''' a method'''
-        Rectangle.number_of_instances -= 1
+        '''a method'''
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
