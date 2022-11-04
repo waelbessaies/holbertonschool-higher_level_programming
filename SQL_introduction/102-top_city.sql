e5a13f0f7b41cc2134a374bc8d3eea1123a3fa6b
root <root@LAPTOP-I8GVERUB.localdomain>
2022-11-04 22:50:36 +0100

    task 102 and 103

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp  DESC LIMIT 3;
