d8d56502b4e476fc0250a91b490f525b527f4e25
root <root@LAPTOP-I8GVERUB.localdomain>
2022-11-08 17:49:54 +0100

    15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -3,3 +3,4 @@ SELECT tv_shows.title
 FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows  ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
