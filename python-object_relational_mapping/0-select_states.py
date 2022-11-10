174d3b508045a4d355ab7beb02ed227c3ac104b0
root <root@LAPTOP-I8GVERUB.localdomain>
2022-11-10 18:31:33 +0100

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    databse = mysql.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        databse=argv[3])
+    cursor = databse.cursor()
+    cursor.execute("SELECT * FROM states")
+    for i in cursor:
+        print(i)
+    cursor.close()
+    databse.close()
