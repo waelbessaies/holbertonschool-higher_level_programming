1a08437e528c4fa31e87792788214a0583c08530
root <root@LAPTOP-I8GVERUB.localdomain>
2022-11-10 19:12:54 +0100

    a script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" a script that lists all states from the database hbtn_0e_0_usa"""
+""" a script that lists all states from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 from sys import argv
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306,
+                       user="root", passwd="root", db="my_db", charset="utf8")
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 query_rows = cur.fetchall()
