8f7ec6aaf2758fc34b4f4dbaf9e158ffa0435d60
root <root@LAPTOP-I8GVERUB.localdomain>
2022-11-10 18:38:11 +0100

    a script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    databse = mysql.connect(
+    databse =  MySQLdb.connect(
         host="localhost",
         port=3306,
         user=argv[1],
