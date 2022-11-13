fa2f0a0a2029cf43db2a133d4c9687dffaf061d7
root <root@LAPTOP-I8GVERUB.localdomain>
2022-11-13 02:05:39 +0100

    base model

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,6 +22,20 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """""JSON string to file"""""
+        filename = "{}.json".format(cls.__name__)
+        list_dict = []
+        if not list_objs:
+            pass
+        else:
+            for j in range(len(list_objs)):
+                list_dict.append(list_objs[j].to_dictionary())
+        lists = cls.to_json_string(list_dict)
+        with open(filename, 'w', encoding="utf-8") as f:
+            f.write(lists)
+
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string:"""
