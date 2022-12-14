ad15c297b42bf6430cf0c74d61201630c5cf4e15
root <root@LAPTOP-I8GVERUB.localdomain>
2022-12-14 17:30:14 +0100

    js

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+    let j = 0;
+    for (let i = 0; list.length > i; i++) {
+      if (list[i] === searchElement) {
+     j++;
+      }
+    }
+    return j;
+  };
