cb07875d6725894be730c7dd25d7b3d8a8185d23
root <root@LAPTOP-I8GVERUB.localdomain>
2022-12-15 15:09:18 +0100

    js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const content = 'Some content!';
-
-fs.writeFile(process.argv[2], process.argv[3],'utf8', content, err => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
+    return console.log(err);
   }
   // fichier écrit avec succès
 });
