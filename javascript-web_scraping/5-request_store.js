28efbaefa4bfc7dedd2bac09c61263767ab09e95
root <root@LAPTOP-I8GVERUB.localdomain>
2022-12-16 20:59:40 +0100

    Write a script that gets the contents of a webpage and stores it in a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], function (_err, response, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
